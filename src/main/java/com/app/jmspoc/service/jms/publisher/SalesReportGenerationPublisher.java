fe014b6d7c385748e407dfd991dc51457fb5256e
zim0101 <cybertronix.4406@gmail.com>
2024-01-25 19:16:12 +0600

    Add message queue publisher and subscriber for sales report generation

--- src/main/java/com/app/jmspoc/service/jms/publisher/SalesReportGenerationPublisher.java
+++ src/main/java/com/app/jmspoc/service/jms/publisher/SalesReportGenerationPublisher.java
@@ -1,4 +1,23 @@
 package com.app.jmspoc.service.jms.publisher;
 
-class SalesReportGenerationPublisher {
+import com.app.jmspoc.model.Product;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SalesReportGenerationPublisher {
+
+    @Value("${jmspoc.queue.sales-report-generation}")
+    private String salesReportGenerationQueue;
+
+    private final JmsTemplate jmsTemplate;
+
+    public SalesReportGenerationPublisher(JmsTemplate jmsTemplate) {
+        this.jmsTemplate = jmsTemplate;
+    }
+
+    public void publishMessage(Product product) {
+        jmsTemplate.convertAndSend(salesReportGenerationQueue, product);
+    }
 }
--- src/main/java/com/app/jmspoc/service/jms/subscriber/SalesReportGenerationSubscriber.java
+++ src/main/java/com/app/jmspoc/service/jms/subscriber/SalesReportGenerationSubscriber.java
@@ -1,4 +1,21 @@
 package com.app.jmspoc.service.jms.subscriber;
 
-class SalesReportGenerationSubscriber {
+import com.app.jmspoc.model.Product;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SalesReportGenerationSubscriber {
+
+    private static final Logger log = LoggerFactory.getLogger(SalesReportGenerationSubscriber.class);
+
+    @JmsListener(destination = "${jmspoc.queue.sales-report-generation}")
+    public void subscribeMessage(Product product) {
+        System.out.println("SalesReportGenerationSubscriber received product: " + product);
+        log.info("---------------------------------------------------");
+        log.info("SalesReportGenerationSubscriber received product: {}", product.getName());
+        log.info("---------------------------------------------------");
+    }
 }
