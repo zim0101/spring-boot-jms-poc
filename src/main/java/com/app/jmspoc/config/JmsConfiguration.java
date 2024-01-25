09c57fa06dae897ee1458b236e43113c1bc2a1cb
zim0101 <cybertronix.4406@gmail.com>
2024-01-25 19:15:27 +0600

    Add jms configuration

--- src/main/java/com/app/jmspoc/config/JmsConfiguration.java
+++ src/main/java/com/app/jmspoc/config/JmsConfiguration.java
@@ -1,4 +1,43 @@
 package com.app.jmspoc.config;
 
-class JmsConfiguration {
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.connection.CachingConnectionFactory;
+import org.springframework.jms.core.JmsTemplate;
+import java.util.Arrays;
+
+@Configuration
+@EnableJms
+public class JmsConfiguration {
+    @Value("${spring.activemq.broker-url}")
+    private String brokerUrl;
+
+    @Value("${spring.activemq.user}")
+    private String username;
+
+    @Value("${spring.activemq.password}")
+    private String password;
+
+    @Bean
+    public CachingConnectionFactory cachingConnectionFactory() {
+        CachingConnectionFactory cachingConnectionFactory = new CachingConnectionFactory();
+        ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory(brokerUrl);
+
+        activeMQConnectionFactory.setUserName(username);
+        activeMQConnectionFactory.setPassword(password);
+        activeMQConnectionFactory.setTrustAllPackages(false);
+        activeMQConnectionFactory.setTrustedPackages(Arrays.asList("com.app.jmspoc.model", "com.app.jmspoc.dto", "java.lang"));
+
+        cachingConnectionFactory.setTargetConnectionFactory(activeMQConnectionFactory);
+
+        return cachingConnectionFactory;
+    }
+
+    @Bean
+    public JmsTemplate jmsTemplate() {
+        return new JmsTemplate(cachingConnectionFactory());
+    }
 }
