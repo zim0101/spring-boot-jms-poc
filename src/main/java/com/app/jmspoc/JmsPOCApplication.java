78af99b6ff94a19025a089f7956e876aa13855aa
zim0101 <cybertronix.4406@gmail.com>
2024-01-25 19:16:46 +0600

    Update application name

--- src/main/java/com/app/jmspoc/JmsPOCApplication.java
+++ src/main/java/com/app/jmspoc/JmsPOCApplication.java
@@ -1,13 +1,34 @@
 package com.app.jmspoc;
 
+import com.app.jmspoc.model.Product;
+import com.app.jmspoc.repository.ProductRepository;
+import com.app.jmspoc.service.jms.publisher.SalesReportGenerationPublisher;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jms.annotation.EnableJms;
 
 @SpringBootApplication
-public class JmspocApplication {
+public class JmsPOCApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JmspocApplication.class, args);
+	private final SalesReportGenerationPublisher salesReportGenerationPublisher;
+	private final ProductRepository productRepository;
+
+    public JmsPOCApplication(SalesReportGenerationPublisher salesReportGenerationPublisher, ProductRepository productRepository) {
+        this.salesReportGenerationPublisher = salesReportGenerationPublisher;
+        this.productRepository = productRepository;
+    }
+
+    public static void main(String[] args) {
+		SpringApplication.run(JmsPOCApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Product product = new Product();
+		product.setName("ABC");
+		product.setPrice("10");
+		productRepository.save(product);
+		salesReportGenerationPublisher.publishMessage(product);
+	}
 }
